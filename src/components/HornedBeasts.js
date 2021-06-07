c8ef30b66c49607cb0b13c5e526cb5522aaff508
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-06-07 15:35:59 +0300

    adding cards

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -3,24 +3,24 @@ import React from 'react';
 class HornedBeasts extends React.Component{
     constructor(props){
         super(props);
-        this.fav = {
+        this.state = {
          numOfFav:0
         }
 
     }
     incrementNumOfFav = () =>{
         this.setState({
-            numOfFav:this.fav.numOfFav+1
+            numOfFav:this.state.numOfFav +1
         })
     }
     render(){
         return(
             <div className='horned'>
                 <h2>{this.props.title}</h2>
-                <img onClick={this.fav.numOfFav} src={this.props.link} alt={this.props.title} title={this.props.title} />
+                <img onClick={this.incrementNumOfFav} src={this.props.link} alt={this.props.title} title={this.props.title} />
                 
                 <p>{this.props.description}</p>
-                <p>Choose it as My Favorite</p>
+                <p>Choose it as My Favorite: {this.state.numOfFav}</p>
                 <button>See More of this</button>
             </div>
         )
