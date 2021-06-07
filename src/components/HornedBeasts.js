0c031a95484074781e9df107520be0ca2ca38861
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-06-07 14:42:08 +0300

    adding more updates

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 class HornedBeasts extends React.Component{
+    constructor(props){
+        super(props);
+        this.fav = {
+         numOfFav:0
+        }
+
+    }
+    incrementNumOfFav = () =>{
+        this.setState({
+            numOfFav:this.fav.numOfFav+1
+        })
+    }
     render(){
         return(
             <div className='horned'>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.link} alt={this.props.title} title={this.props.title} />
+                <img onClick={this.fav.numOfFav} src={this.props.link} alt={this.props.title} title={this.props.title} />
                 
                 <p>{this.props.description}</p>
+                <p>Choose it as My Favorite</p>
+                <button>See More of this</button>
             </div>
         )
     }
